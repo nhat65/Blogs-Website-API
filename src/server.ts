82ca4310a9c8c8dc3493f4727d1efb7cdcd8b9a6
Nhat <phantannhat12@gmail.com>
2025-07-18 09:14:57 +0700

    Init project and create login

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import 'dotenv/config'
+
+import authRouter from './router/authRouter'
+
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
