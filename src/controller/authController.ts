16aa22965d227ebb8ef437ed78f08c4d7cba421b
Nhat <phantannhat12@gmail.com>
2025-07-18 20:37:23 +0700

    create logout

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -119,3 +119,10 @@ export const login = async (req: Request, res: Response): Promise<void> => {
      });
    }
  };
+
+export const logout = async (_req: Request, res: Response): Promise<void> => {
+  res
+    .clearCookie('Authorization')
+    .status(200)
+    .json({ success: true, message: 'Logout successfully!' });
+};
--- src/router/authRouter.ts
+++ src/router/authRouter.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import * as authController from '../controller/authController';
 import { validateLogin, validateRegister } from '../middleware/validate';
+import { identifier } from '../middleware/identify';
 
 const router: Router = express.Router();
 
@@ -9,5 +10,8 @@ router.post('/login',validateLogin ,authController.login);
 
  //Router handle register
  router.post('/register', validateRegister, authController.register);
+
+//Router handle logout
+router.post('/logout', identifier, authController.logout)
 
 export default router;
